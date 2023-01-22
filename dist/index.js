df28d3a4b9c01fa371a2757762ad5b94168b9f82
Kher Phay Chang <kayphay92@gmail.com>
2023-01-23 00:02:52 +0800

    chore(jira): update version

--- dist/index.js
+++ dist/index.js
@@ -184,13 +184,14 @@ class Jira {
 		};
 
 		try {
-      console.log(state)
+      console.log(state.req.body)
 			await client(state, `${serviceName}:${apiMethodName}`);
 		} catch (error) {
 			const fields = {
 				originError: error,
 				source: "jira",
 			};
+      console.log(JSON.stringify(error))
 
 			delete state.req.headers;
 
