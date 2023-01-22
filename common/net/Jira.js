d8e26323f61867f2f44eb7072a4b50b7c36a1051
Kher Phay Chang <kayphay92@gmail.com>
2023-01-23 00:24:31 +0800

    bug(jira): fix update issue

--- common/net/Jira.js
+++ common/net/Jira.js
@@ -26,17 +26,19 @@ class Jira {
 
 	async updateField(issueId, data) {
 		try {
-			const array = data.split(',');
-      const parsedData = array.reduce((newData, item) => {
-        const splitItem = item.split('::');
-        if (splitItem.length === 2) {
-          newData[splitItem[0].trim()] = splitItem[1];
-        }
-        return newData;
-      }, {});
-      const body = JSON.parse(JSON.stringify({
-        fields: parsedData
-      }));
+			const array = data.split(",");
+			const parsedData = array.reduce((newData, item) => {
+				const splitItem = item.split("::");
+				if (splitItem.length === 2) {
+					newData[splitItem[0].trim()] = splitItem[1];
+				}
+				return newData;
+			}, {});
+			const body = JSON.parse(
+				JSON.stringify({
+					fields: parsedData,
+				})
+			);
 			return this.fetch(
 				"updateField",
 				{
@@ -45,8 +47,7 @@ class Jira {
 				{
 					method: "PUT",
 					body,
-				},
-        true
+				}
 			);
 		} catch (err) {
 			throw err;
@@ -65,15 +66,15 @@ class Jira {
 		const { fields = [], expand = [] } = query;
 
 		try {
-			return this.fetch('getIssue', {
+			return this.fetch("getIssue", {
 				pathname: `/rest/api/2/issue/${issueId}`,
 				query: {
-					fields: fields.join(','),
-					expand: expand.join(','),
+					fields: fields.join(","),
+					expand: expand.join(","),
 				},
 			});
 		} catch (error) {
-			if (get(error, 'res.status') === 404) {
+			if (get(error, "res.status") === 404) {
 				return;
 			}
 
@@ -83,24 +84,24 @@ class Jira {
 
 	async getIssueTransitions(issueId) {
 		return this.fetch(
-			'getIssueTransitions',
+			"getIssueTransitions",
 			{
 				pathname: `/rest/api/2/issue/${issueId}/transitions`,
 			},
 			{
-				method: 'GET',
+				method: "GET",
 			}
 		);
 	}
 
 	async transitionIssue(issueId, data) {
 		return this.fetch(
-			'transitionIssue',
+			"transitionIssue",
 			{
 				pathname: `/rest/api/3/issue/${issueId}/transitions`,
 			},
 			{
-				method: 'POST',
+				method: "POST",
 				body: data,
 			}
 		);
@@ -110,7 +111,7 @@ class Jira {
 		apiMethodName,
 		{ host, pathname, query },
 		{ method, body, headers = {} } = {},
-    skipStringify
+		skipStringify
 	) {
 		const url = format({
 			host: host || this.baseUrl,
@@ -119,22 +120,26 @@ class Jira {
 		});
 
 		if (!method) {
-			method = 'GET';
+			method = "GET";
 		}
 
-		if (headers['Content-Type'] === undefined) {
-			headers['Content-Type'] = 'application/json';
+		if (headers["Content-Type"] === undefined) {
+			headers["Content-Type"] = "application/json";
 		}
 
 		if (headers.Authorization === undefined) {
 			headers.Authorization = `Basic ${Buffer.from(
 				`${this.email}:${this.token}`
-			).toString('base64')}`;
+			).toString("base64")}`;
 		}
 
 		// strong check for undefined
 		// cause body variable can be 'false' boolean value
-		if (body && headers['Content-Type'] === "application/json" && !skipStringify) {
+		if (
+			body &&
+			headers["Content-Type"] === "application/json" &&
+			!skipStringify
+		) {
 			body = JSON.stringify(body);
 		}
 
@@ -148,7 +153,7 @@ class Jira {
 		};
 
 		try {
-      console.log('request:', state.req.body)
+			console.log("request:", state.req.body);
 			await client(state, `${serviceName}:${apiMethodName}`);
 		} catch (error) {
 			const fields = {
@@ -157,9 +162,13 @@ class Jira {
 			};
 
 			delete state.req.headers;
-      const formattedError = Object.assign(new Error("Jira API error"), state.res.body.errorMessages, fields);
+			const formattedError = Object.assign(
+				new Error("Jira API error"),
+				state,
+				fields
+			);
 
-      console.log('#### error', JSON.stringify(formattedError))
+			console.log("#### error", JSON.stringify(formattedError));
 
 			throw formattedError;
 		}
